60551478539598d1abf17ac005ffdeb4f8871580
geekyparas21 <paraskansal21@gmail.com>
2026-01-11 16:58:55 +0530

    Add files via upload

---
+++ lstm_text (1).py
@@ -0,0 +1,131 @@
+# =========================
+# LSTM TEXT GENERATION CODE
+# =========================
+
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense, Dropout
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+import keras.utils as ku
+
+from tensorflow import set_random_seed
+from numpy.random import seed
+
+import pandas as pd
+import numpy as np
+import string, os
+import warnings
+
+warnings.filterwarnings("ignore")
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+set_random_seed(2)
+seed(1)
+
+# -------------------------
+# Load Dataset
+# -------------------------
+curr_dir = '../input/'
+all_headlines = []
+
+for filename in os.listdir(curr_dir):
+    if 'Articles' in filename:
+        article_df = pd.read_csv(curr_dir + filename)
+        all_headlines.extend(list(article_df.headline.values))
+        break
+
+all_headlines = [h for h in all_headlines if h != "Unknown"]
+
+# -------------------------
+# Clean Text
+# -------------------------
+def clean_text(txt):
+    txt = "".join(v for v in txt if v not in string.punctuation).lower()
+    txt = txt.encode("utf8").decode("ascii",'ignore')
+    return txt
+
+corpus = [clean_text(x) for x in all_headlines]
+
+# -------------------------
+# Tokenization & N-grams
+# -------------------------
+tokenizer = Tokenizer()
+
+def get_sequence_of_tokens(corpus):
+    tokenizer.fit_on_texts(corpus)
+    total_words = len(tokenizer.word_index) + 1
+    
+    input_sequences = []
+    for line in corpus:
+        token_list = tokenizer.texts_to_sequences([line])[0]
+        for i in range(1, len(token_list)):
+            input_sequences.append(token_list[:i+1])
+    
+    return input_sequences, total_words
+
+inp_sequences, total_words = get_sequence_of_tokens(corpus)
+
+# -------------------------
+# Padding & Labels
+# -------------------------
+def generate_padded_sequences(input_sequences):
+    max_sequence_len = max(len(x) for x in input_sequences)
+    input_sequences = np.array(
+        pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre')
+    )
+    
+    predictors = input_sequences[:, :-1]
+    label = input_sequences[:, -1]
+    label = ku.to_categorical(label, num_classes=total_words)
+    
+    return predictors, label, max_sequence_len
+
+predictors, label, max_sequence_len = generate_padded_sequences(inp_sequences)
+
+# -------------------------
+# Model
+# -------------------------
+def create_model(max_sequence_len, total_words):
+    input_len = max_sequence_len - 1
+    
+    model = Sequential()
+    model.add(Embedding(total_words, 10, input_length=input_len))
+    model.add(LSTM(100))
+    model.add(Dropout(0.1))
+    model.add(Dense(total_words, activation='softmax'))
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    return model
+
+model = create_model(max_sequence_len, total_words)
+model.summary()
+
+# -------------------------
+# Train
+# -------------------------
+model.fit(predictors, label, epochs=100, verbose=5)
+
+# -------------------------
+# Text Generation
+# -------------------------
+def generate_text(seed_text, next_words, model, max_sequence_len):
+    for _ in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+        
+        predicted = np.argmax(model.predict(token_list, verbose=0), axis=-1)
+        
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        
+        seed_text += " " + output_word
+    
+    return seed_text.title()
+
+# -------------------------
+# Example
+# -------------------------
+print(generate_text("india and china", 5, model, max_sequence_len))
